15eb28ec72f0296f276147e25fbcf21243a8b59c
pavithra-sekar-dev <pavithra24.sekar@gmail.com>
2022-08-22 20:42:55 +0530

    map,foreach, array iterate (in, for loop)

--- methods/script.js
+++ methods/script.js
@@ -33,3 +33,107 @@ for (let [k, v] of Object.entries(person)) {
 }
 
 console.log('**************************');
+// different group of datatype
+//square brackets
+//array -accessing through index
+let ages = [89, 90, 24, 78, '67', {}, []];
+//starting index 0
+//length -3
+let fruits = ['Apple', 'Orange', 'Pineapple'];
+let persons = [
+  {
+    fname: 'ram',
+    lname: 'kumar',
+    mobile: '+90687687',
+    age: 12,
+  },
+  {
+    fname: 'ravi',
+    lname: 'kumar',
+    mobile: '+90687687',
+    age: 67,
+  },
+];
+//iterate
+for (let i in fruits) {
+  console.log(i, fruits[i]);
+}
+//for loop
+//intialization, condn, inc/dec
+for (let i = 0; i < persons.length; i++) {
+  // console.log(persons[i], persons[i].fname);
+  console.log('element -obj', persons[i]);
+  let ele = Object.keys(persons[i]);
+  // ["fname","lname","age"]
+  for (let k of ele) {
+    console.log(k, persons[i][k]);
+  }
+}
+//array arrow function -ECMA 6
+//foreach,filter,some,every,map,reduce
+//forEach -iterating the array ele -parameter(each ele, index, array)
+// () -more than one parameter else optional
+persons.forEach((item, i, arr) => {
+  console.log(item, i, arr);
+});
+fruits.forEach((item) => {
+  console.log(item);
+});
+
+//map -tranformation of data -(items)
+//already existing modify & adding new item
+//return new array
+let modifyPersons = persons.map((item, i) => {
+  return {
+    id: i + 1,
+    firstName: item.fname,
+    isEligiblie: item.age > 18 ? true : false,
+    fullName: item.fname + item.lname,
+  };
+});
+console.log(modifyPersons, persons);
+
+let modPersons = persons.map((item, i) => {
+  return {
+    id: i + 1,
+    isEligiblie: item.age > 18 ? true : false,
+    fname: item.fname,
+    lname: item.lname,
+    age: item.age,
+    mobile: item.mobile,
+  };
+});
+
+console.log(modPersons, persons);
+
+let company = [
+  {
+    name: 'HCL',
+    count: 5000,
+    address: { city: 'chennai', pincode: '57687897' },
+    colors: ['blue', 'red'],
+  },
+  {
+    name: 'Quartz',
+    count: 250,
+    address: { city: 'chennai', pincode: '57687897' },
+    colors: ['blue', 'red'],
+  },
+];
+
+let companyDetails = company.map((item, i) => {
+  return {
+    companyId: i + 1,
+    stageLevel: item.count >= 5000 ? 'MNC' : 'Middle-level',
+    ...item,
+  };
+});
+console.log(companyDetails, company);
+
+//spread operator (...)
+// value clone - one layer -workout
+let language = ['python', 'java'];
+// let fav_language = language; //memory copy
+let fav_language = [...language];
+fav_language[1] = 'Node Js';
+console.log(language, fav_language);
