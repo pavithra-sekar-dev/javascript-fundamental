306b2235ad5c0d3f76e8903cf0876c4a0597d0db
pavithra-sekar-dev <pavithra24.sekar@gmail.com>
2022-08-23 20:57:23 +0530

    2 files modified

--- methods/script.js
+++ methods/script.js
@@ -53,7 +53,20 @@ let persons = [
     mobile: '+90687687',
     age: 67,
   },
+  {
+    fname: 'nisha',
+    lname: 'noori',
+    mobile: 475758697,
+    age: 27,
+  },
+  {
+    fname: 'vinoth',
+    lname: 'kumar',
+    mobile: 475758697,
+    age: 21,
+  },
 ];
+
 //iterate
 for (let i in fruits) {
   console.log('for in ', i, fruits[i]);
@@ -69,6 +82,8 @@ for (let i = 0; i < persons.length; i++) {
     console.log('for loop', k, persons[i][k]);
   }
 }
+console.log('**********************************');
+
 //array arrow function -ECMA 6
 //foreach,filter,some,every,map,reduce
 //forEach -iterating the array ele -parameter(each ele, index, array)
@@ -79,6 +94,7 @@ persons.forEach((item, i, arr) => {
 fruits.forEach((item) => {
   console.log('for each fruits', item);
 });
+console.log('**********************************');
 
 //map -tranformation of data -(items)
 //already existing modify & adding new item
@@ -92,6 +108,7 @@ let modifyPersons = persons.map((item, i) => {
   };
 });
 console.log('map persons', modifyPersons, persons);
+console.log('**********************************');
 
 let modPersons = persons.map((item, i) => {
   return {
@@ -105,6 +122,7 @@ let modPersons = persons.map((item, i) => {
 });
 
 console.log('map persons existing data', modPersons, persons);
+console.log('**********************************');
 
 let company = [
   {
@@ -129,6 +147,7 @@ let companyDetails = company.map((item, i) => {
   };
 });
 console.log('map company spread operators', companyDetails, company);
+console.log('**********************************');
 
 //spread operator (...)
 // value clone - one layer -workout
@@ -137,3 +156,112 @@ let language = ['python', 'java'];
 let fav_language = [...language];
 fav_language[1] = 'Node Js';
 console.log(language, fav_language);
+console.log('**********************************');
+
+//filter -> (condn satisfied true return value )for,if push
+// let eligiblePerson = persons.filter((item) => {
+//   return item.age >= 50;
+// });
+//single
+let eligiblePerson = persons.filter((item) => item.lname[0] == 'n');
+console.log('eligibilePerson', eligiblePerson);
+
+//some -> array -> return true/false  -  anyone ele should satisfied the condn in array
+//loginDetails 10000 oru person login sucess
+//example search -> any column one filed match return value.
+let loginDetails = [
+  { id: 1, uname: 'abc', pwd: 'abc123' },
+  { id: 2, uname: 'agent', pwd: 'agent123' },
+  { id: 3, uname: 'admin', pwd: 'admin123' },
+];
+let checkLogin = loginDetails.some(
+  (item) => item.uname == 'admin' && item.pwd == 'admin123'
+);
+console.log(checkLogin);
+console.log('**********************************');
+
+// every -opps some -  all ele should satisfied the condn in array -return true / false;
+// input field all valid  -proceed
+
+let validField = ['t', 't', 'f', 't', 't'];
+let checkStatus = validField.every((item) => item == 't' || item == 'f');
+console.log(checkStatus);
+console.log('**********************************');
+
+//reduce -> return single value -> arithmetric operation
+//sumofnum, multiofnum, concat
+let numbers = [9, 7, 6, 5];
+let sum = numbers.reduce((prev, curr, i, arr) => {
+  console.log(prev, curr, i, arr);
+  return prev + curr;
+});
+console.log(sum);
+console.log('**********************************');
+
+//combination -filter -map
+let personDetails = persons
+  .filter((item) => item.age > 21)
+  .map((data) => {
+    return {
+      ...data,
+      isEligiblie: true,
+    };
+  });
+console.log(personDetails);
+console.log('**********************************');
+
+//array methods
+let names = ['nisha', 'vinoth'];
+//push ->add last position -multiple
+names.push('mani', 'ureka');
+console.log('push', names);
+console.log('**********************************');
+//pop -remove -last posi elem - one ele
+names.pop();
+console.log('pop', names);
+console.log('**********************************');
+//shift - first pos remove -one ele
+names.shift();
+console.log('shift', names);
+console.log('**********************************');
+//unshift -> first pos add -multiple
+names.unshift('arun', 'kaviya', 'ramya');
+console.log('unshift', names);
+console.log('**********************************');
+//reverse ->ele reverse
+names.reverse();
+console.log('reverse', names);
+console.log('**********************************');
+//slice- extracting part
+let extractPart = names.slice(0, 3);
+console.log('slice', extractPart, names);
+console.log('**********************************');
+//splice - add & remove particular position (index)
+//syntax: splice(startingIndex,deletecount,addele(optional))
+names.splice(2, 0, 'pavithra', 'vasanthi');
+console.log('addElem -splice', names);
+names.splice(1, 2);
+console.log('remove -splice', names);
+names.splice(3, 1, 'sathiya', 'pragathi');
+console.log('add & remove -splice', names);
+//find
+console.log(names.find((item) => item == 'sathiya'));
+
+//findIndex
+console.log(names.findIndex((item) => item == 'sathiya'));
+
+//let numbers=[90,89,67,90,67]-duplicate removed
+let selectedvalues = [90, 89, 67, 90, 67];
+let result = [];
+selectedvalues.forEach((item) => {
+  console.log(item);
+  let index = result.findIndex((r) => r == item);
+  console.log(index);
+  if (index == -1) {
+    result.push(item);
+  }
+});
+console.log(result);
+
+// palindrome -madam ,-for loop-dec
+// "developer" -> ["de","ve","lo","pe","r"]
